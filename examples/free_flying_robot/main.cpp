5df5a50b651564b30803d558214fc1e21231586b
juanjqo <juanjqogm@gmail.com>
2024-11-19 13:47:46 +0000

    added another example

--- examples/free_flying_robot/main.cpp
+++ examples/free_flying_robot/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <dqrobotics/DQ.h>
-#include <dqrobotics/interfaces/coppeliasim/DQ_CoppeliaSimZmqInterface.h>
+#include <dqrobotics/interfaces/coppeliasim/DQ_CoppeliaSimInterfaceZMQ.h>
 #include <memory>
 
 using namespace DQ_robotics;
@@ -8,9 +8,9 @@ using namespace Eigen;
 
 int main()
 {
-    auto vi = std::make_shared<DQ_CoppeliaSimZmqInterface>();
+    auto vi = std::make_shared<DQ_CoppeliaSimInterfaceZMQ>();
     vi->connect();
-    auto vi_exp = std::make_shared<DQ_CoppeliaSimZmqInterface::experimental>(vi);
+    auto vi_exp = std::make_shared<DQ_CoppeliaSimInterfaceZMQ::experimental>(vi);
     vi_exp->close_scene();
 
 
@@ -18,7 +18,7 @@ int main()
     vi->set_stepping_mode(true);
 
     std::string robotname = "/Sphere";
-    vi_exp->add_primitive(DQ_CoppeliaSimZmqInterface::PRIMITIVE::SPHEROID, robotname, {0.2, 0.2, 0.2});
+    vi_exp->add_primitive(DQ_CoppeliaSimInterfaceZMQ::PRIMITIVE::SPHEROID, robotname, {0.2, 0.2, 0.2});
     vi->set_object_pose(robotname, 1 + 0.5*E_*0.3*k_);
     vi_exp->set_object_color(robotname, {1, 0,0,0.5});
     vi_exp->set_object_as_static(robotname, false);
@@ -40,13 +40,13 @@ int main()
         DQ twist_a = x*twist_b*x.conj();
 
         vi_exp->set_twist(robotname, twist_b,
-                          DQ_CoppeliaSimZmqInterface::REFERENCE::BODY_FRAME);
+                          DQ_CoppeliaSimInterfaceZMQ::REFERENCE::BODY_FRAME);
 
         std::cout<<"twist_a :    "<<twist_a<<std::endl;
         std::cout<<"twist_a_:    "<<vi_exp->get_twist(robotname)<<std::endl;
         std::cout<<" "<<std::endl;
         std::cout<<"twist_b :    "<<twist_b<<std::endl;
-        std::cout<<"twist_b_:    "<<vi_exp->get_twist(robotname, DQ_CoppeliaSimZmqInterface::REFERENCE::BODY_FRAME)<<std::endl;
+        std::cout<<"twist_b_:    "<<vi_exp->get_twist(robotname, DQ_CoppeliaSimInterfaceZMQ::REFERENCE::BODY_FRAME)<<std::endl;
 
 
         vi->trigger_next_simulation_step();
