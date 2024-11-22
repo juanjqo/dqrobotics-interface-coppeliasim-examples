5f71359e56e59336367d95fe3b4e19545593ec58
Juan <juanjqogm@gmail.com>
2024-11-22 10:35:03 +0000

    updated the free flying robot to comply the current dqrobotics branch

--- examples/free_flying_robot/main.cpp
+++ examples/free_flying_robot/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <dqrobotics/DQ.h>
-#include <dqrobotics/interfaces/coppeliasim/DQ_CoppeliaSimInterfaceZMQ.h>
+#include <dqrobotics/interfaces/coppeliasim/DQ_CoppeliaSimInterfaceZMQExperimental.h>
 #include <memory>
 
 using namespace DQ_robotics;
@@ -8,26 +8,25 @@ using namespace Eigen;
 
 int main()
 {
-    auto vi = std::make_shared<DQ_CoppeliaSimInterfaceZMQ>();
-    vi->connect();
-    auto vi_exp = std::make_shared<DQ_CoppeliaSimInterfaceZMQ::experimental>(vi);
-    vi_exp->close_scene();
+    auto vi = std::make_shared<DQ_CoppeliaSimInterfaceZMQExperimental>();
+    vi->connect("localhost", 23000, 400);
+    vi->close_scene();
 
 
-    vi_exp->set_gravity(DQ(0));
+    vi->set_gravity(DQ(0));
     vi->set_stepping_mode(true);
 
     std::string robotname = "/Sphere";
-    vi_exp->add_primitive(DQ_CoppeliaSimInterfaceZMQ::PRIMITIVE::SPHEROID, robotname, {0.2, 0.2, 0.2});
+    vi->add_primitive(DQ_CoppeliaSimInterfaceZMQExperimental::PRIMITIVE::SPHEROID, robotname, {0.2, 0.2, 0.2});
     vi->set_object_pose(robotname, 1 + 0.5*E_*0.3*k_);
-    vi_exp->set_object_color(robotname, {1, 0,0,0.5});
-    vi_exp->set_object_as_static(robotname, false);
-    //vi_exp->se
+    vi->set_object_color(robotname, {1, 0,0,0.5});
+    vi->set_object_as_static(robotname, false);
+
 
 
     vi->start_simulation();
 
-    for (int i=0; i<200; i++)
+    for (int i=0; i<2000; i++)
     {
         DQ x = vi->get_object_pose(robotname);
 
@@ -39,14 +38,14 @@ int main()
         DQ twist_b = w + E_*(p_dot);
         DQ twist_a = x*twist_b*x.conj();
 
-        vi_exp->set_twist(robotname, twist_b,
-                          DQ_CoppeliaSimInterfaceZMQ::REFERENCE::BODY_FRAME);
+        vi->set_twist(robotname, twist_b,
+                          DQ_CoppeliaSimInterfaceZMQExperimental::REFERENCE::BODY_FRAME);
 
         std::cout<<"twist_a :    "<<twist_a<<std::endl;
-        std::cout<<"twist_a_:    "<<vi_exp->get_twist(robotname)<<std::endl;
+        std::cout<<"twist_a_:    "<<vi->get_twist(robotname)<<std::endl;
         std::cout<<" "<<std::endl;
         std::cout<<"twist_b :    "<<twist_b<<std::endl;
-        std::cout<<"twist_b_:    "<<vi_exp->get_twist(robotname, DQ_CoppeliaSimInterfaceZMQ::REFERENCE::BODY_FRAME)<<std::endl;
+        std::cout<<"twist_b_:    "<<vi->get_twist(robotname, DQ_CoppeliaSimInterfaceZMQExperimental::REFERENCE::BODY_FRAME)<<std::endl;
 
 
         vi->trigger_next_simulation_step();
